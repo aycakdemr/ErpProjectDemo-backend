fb8ebb34b345dfc31597f8c3ace3c6d0ef264172
Ayça Akdemir <mabeaaa@gmail.com>
2022-07-21 13:42:34 +0300

    core katmanı oluşturuldu ve dataaccess katmanında interfaceler tanımlandı

---
+++ DataAccessLayer/Abtract/ISectionDal.cs
@@ -0,0 +1,14 @@
+﻿using CoreLayer;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abtract
+{
+    public interface ISectionDal : IEntityRepository<Section>
+    {
+    }
+}
