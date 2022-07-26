7e426ff8c0042815396f2ba092c4808ec3fa560a
Ayça Akdemir <mabeaaa@gmail.com>
2022-07-26 10:53:35 +0300

    DataResult ve Result yapısı kuruldu.

---
+++ BusinessLayer/Abstract/IService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IService<T>
+    {
+        //IResult Add(T t);
+        //IResult Delete(int id);
+        //IResult Update(T t);
+        //IDataResult<List<T>> GetAll();
+        //IDataResult<List<T>> GetById(int id);
+    }
+}
