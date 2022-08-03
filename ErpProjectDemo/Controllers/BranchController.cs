9eb6b2bd279ef9c99379c350dfa78eade4d23094
Ayça Akdemir <mabeaaa@gmail.com>
2022-08-03 17:00:15 +0300

    Branch işlemleri tamamlandı

---
+++ ErpProjectDemo/Controllers/BranchController.cs
@@ -0,0 +1,52 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.NpgSql;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ErpProjectDemo.Controllers
+{
+    public class BranchController : Controller
+    {
+        BranchManager bm = new BranchManager(new EfBranchDal());
+        public IActionResult Index()
+        {
+            var val = bm.GetAll().Data;
+            return View(val);
+        }
+        public IActionResult DeleteBranch(int id)
+        {
+            bm.Delete(id);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult UpdateBranch(int id)
+        {
+            var value = bm.GetById(id).Data.FirstOrDefault();
+            return View(value);
+        }
+        [HttpPost]
+        public IActionResult UpdateBranch(Branch w)
+        {
+            bm.Update(w);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult AddBranch()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddBranch(Branch s)
+        {
+           
+                bm.Add(s);
+                return RedirectToAction("Index");
+           
+        }
+    }
+}
